69bfceb1a3ce0c94d7b773758af984dea04e3ce2
Jeff Wang <Jeff.wang@ihcstas.au>
2022-12-19 09:49:21 +1100

    update gui

--- PSGUI.ps1
+++ PSGUI.ps1
@@ -4,4 +4,50 @@ $main_form.Text = 'GUI for Powershell Silly Mode'
 $main_form.Width = 800
 $main_form.Height = 800
 $main_form.AutoSize = $true
-$main_form.ShowDialog()
+
+
+#Sign in ExchangeOnline 
+
+<#
+$answer = [System.Windows.MessageBox]::Show( "Do you want to login your ExchangeOnline?", " Login Confirmation", "YesNoCancel", "Warning" )
+$Confirm= Read-Host Do you want to login your ExchangeOnline [Y] Yes [N] No  
+ 		 if($Confirm -match "[yY]")  
+ 		 {  
+ 			 Write-host "Please put your Username & Password in the pop up window to login Exchange Online PowerShell module" 
+             Write-Host Connecting to Exchange Online...
+             Connect-ExchangeOnline 
+             Write-Host You have login successfully!`r`n
+             $a = 2 
+ 		 }  
+ 		 else  
+ 		 {   
+            Write-Output '  Thank you for using PowerShell Silly Mode
+  	See you next time!'	 
+             }
+#>
+
+#Create Label 
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = 'All Mailbox'
+$Label.Location = New-Object System.Drawing.Point(0,10)
+$Label.AutoSize = $true
+$main_form.Controls.Add($Label)
+
+$ComboBox = New-Object System.Windows.Forms.ComboBox
+$ComboBox.Width = 300
+$Users = Get-Mailbox -filter * -Properties SamAccountName
+Foreach ($User in $Users)
+{
+$ComboBox.Items.Add($User.SamAccountName);
+}
+$ComboBox.Location  = New-Object System.Drawing.Point(60,10)
+$main_form.Controls.Add($ComboBox)
+
+
+
+
+
+
+
+
+$main_form.ShowDialog() #This line will show GUI, add code before this line
